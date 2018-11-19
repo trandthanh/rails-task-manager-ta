6b2b430e1aa63d76d66dc0efdb308aea9032a77e
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-11-19 20:03:36 +0100

    Finished ex.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'tasks', to: "tasks#index"
-  get 'tasks/new', to: "tasks#new", as: :new_task
-  post 'tasks', to: "tasks#create"
-
-  get "tasks/:id", to: "tasks#show", as: :task
-
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
-  patch "tasks/:id", to: "tasks#update"
-
-  delete "tasks/:id", to: "tasks#destroy"
-
   root to: "tasks#index"
+
+  # get 'tasks', to: "tasks#index"
+  # get 'tasks/new', to: "tasks#new", as: :new_task
+  # post 'tasks', to: "tasks#create"
+
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
+  resources :tasks
 end
